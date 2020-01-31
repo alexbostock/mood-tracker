46142ba845fc2dbd8483dd53c0319dd865d8740b
Alex Bostock <alex@bostock.uk>
2020-01-31 22:20:26 +0000

    Refactor transform functions for readability

--- store/transforms.ts
+++ store/transforms.ts
@@ -7,17 +7,14 @@ export function outboundSetTransform<U, T>(childTransform: (val: U) => T) {
 }
 
 export function inboundMapTransform<K, T, U>(childTransform: (val: T) => U) {
-  return (inbound: Map<K, T>): Array<[K, U]> => Array.from(inbound.entries()).map(entry => {
-    const [key, val] = entry;
-    return [key, childTransform(val)];
-  });
+  return (inbound: Map<K, T>): Array<[K, U]> => Array.from(inbound.entries())
+    .map(([key, val]) => [key, childTransform(val)]);
 }
 
 export function outboundMapTransform<K, U, T>(childTransform: (val: U) => T) {
   return (outbound: Array<[K, U]>): Map<K, T> => {
     const map = new Map();
-    for (const entry of outbound) {
-      const [key, val] = entry;
+    for (const [key, val] of outbound) {
       map.set(key, childTransform(val));
     }
     return map;
